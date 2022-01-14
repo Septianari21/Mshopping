5fb2b30504f32fcfa333eff2629139b1e10c5804
ArjunInformatika <97738547+ArjunInformatika@users.noreply.github.com>
2022-01-14 20:39:21 +0800

    footer.php

--- footer.php
+++ footer.php
@@ -27,7 +27,7 @@
 							</ul>
 							<span class="copyright">
 								<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-								Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved
+								Copyright & Copy;<script>document.write(new Date().getFullYear());</script> All Rights Reserved
 							
 							</span>
 						</div>
